dae2cf068baa827e68005ec1e16410b2421f1235
miguel <tsambamiguelito@gmail.com>
2020-11-22 19:58:25 +0100

    Initial commit

---
+++ src/main/java/fr/myt/learning/petclinicclone/PetclinicCloneApplication.java
@@ -0,0 +1,13 @@
+package fr.myt.learning.petclinicclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/myt/learning/petclinicclone/PetclinicCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.myt.learning.petclinicclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
