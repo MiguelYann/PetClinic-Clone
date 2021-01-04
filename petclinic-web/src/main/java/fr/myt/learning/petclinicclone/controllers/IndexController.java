8e3b8f7b2d40fe73c6aeb9a64fda1f712f98328d
miguel <tsambamiguelito@gmail.com>
2021-01-05 00:32:40 +0100

    feat: Create index page with template thymeleaf. Closes #7

---
+++ petclinic-web/src/main/java/fr/myt/learning/petclinicclone/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package fr.myt.learning.petclinicclone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+  @RequestMapping({"", "/" , "index"})
+  public String index() {
+    return "index";
+  }
+}
