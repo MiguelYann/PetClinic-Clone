4554cd7da00fa100bc27be58aa03592c5b94efc7
miguel <tsambamiguelito@gmail.com>
2021-02-06 19:49:15 +0100

    List all owners. Closes #26.

--- petclinic-web/src/main/java/fr/myt/learning/petclinicclone/controllers/OwnerController.java
+++ petclinic-web/src/main/java/fr/myt/learning/petclinicclone/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package fr.myt.learning.petclinicclone.controllers;
 
+import fr.myt.learning.petclinicclone.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-  @RequestMapping({ "", "index" })
-  public String getAllOwner() {
-    return "owners/index";
-  }
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String getAllOwner(Model model) {
+        System.out.println(model);
+        model.addAttribute("owners", ownerService.findAll());
+        System.out.println(model);
+
+        return "owners/index";
+    }
 }
