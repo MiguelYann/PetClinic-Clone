c02372b92e51269e0b9ee25a2010599d5d59f313
miguel <tsambamiguelito@gmail.com>
2021-02-06 19:50:04 +0100

    List all vets. Closes #22.

--- petclinic-web/src/main/java/fr/myt/learning/petclinicclone/controllers/VetController.java
+++ petclinic-web/src/main/java/fr/myt/learning/petclinicclone/controllers/VetController.java
@@ -1,16 +1,23 @@
 package fr.myt.learning.petclinicclone.controllers;
 
-import javax.annotation.Resource;
-
+import fr.myt.learning.petclinicclone.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-  @RequestMapping({ "", "/index" })
-  public String getAllVets() {
-    return "vets/index";
-  }
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/index"})
+    public String getAllVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
 }
